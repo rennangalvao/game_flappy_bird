d171050ecb4966e42dde96dc1d0075bd77d683a1
rennangalvao <inc.rennan@gmail.com>
2022-02-04 21:42:09 -0500

    Exibindo os Canos

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
  FLAPPY BIRD
+ 
+ 
  Ultilizando a biblioteca LibGDX - https://libgdx.com/
- na criação do projetos.
+ na criação do app.
 
  Passo a passo de criação.
  https://libgdx.com/wiki/start/project-generation
--- core/src/dev/rennangalvao/flappybird/FlappyBird.java
+++ core/src/dev/rennangalvao/flappybird/FlappyBird.java
@@ -6,11 +6,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class FlappyBird extends ApplicationAdapter {
 
 	private SpriteBatch batch;
 	private  Texture[] passaros;
 	private Texture fundo;
+	private Texture canoBaixo;
+	private Texture canoTopo;
+	private Texture canoBaixoMaior;
+	private Texture canoTopoMaior;
+	private Random numerosRandomico;
 
 	// Atributos de configuração
 	private int larguraDispositivo;
@@ -19,6 +26,12 @@ public class FlappyBird extends ApplicationAdapter {
 	private float variacao = 0;
 	private float velocidadeQueda = 0;
 	private float posicaoInicialVertical;
+	private float posicaoMovimentoCanoHorizontal;
+	private float espacoEntreCanos;
+
+	private float deltaTime;
+
+	private float alturaEntreCanosRondomica;
 
 
 
@@ -27,6 +40,8 @@ public class FlappyBird extends ApplicationAdapter {
 
 		batch = new SpriteBatch();
 
+		numerosRandomico = new Random();
+
 		passaros = new Texture[3];
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
@@ -34,17 +49,31 @@ public class FlappyBird extends ApplicationAdapter {
 
 		fundo = new Texture("fundo.png");
 
+		canoBaixoMaior = new Texture("cano_baixo_maior.png");
+		canoTopoMaior = new Texture("cano_topo_maior.png");
+		canoBaixo = new Texture("cano_baixo.png");
+		canoTopo = new Texture("cano_topo.png");
+
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo  = Gdx.graphics.getHeight();
 		posicaoInicialVertical = alturaDispositivo / 2;
+		posicaoMovimentoCanoHorizontal = larguraDispositivo - 100;
+		espacoEntreCanos = 300;
+
+
 	}
 
 	@Override
 	public void render () { // loop
 
+		deltaTime = Gdx.graphics.getDeltaTime();
+
 		// Incremento
-		variacao += Gdx.graphics.getDeltaTime() * 10;
+		variacao += deltaTime * 10;
 		if(variacao > 2) variacao = 0;
+
+		//Decrementar
+		posicaoMovimentoCanoHorizontal -= deltaTime * 200;
 
 		//Velocidade
 		velocidadeQueda ++;
@@ -53,9 +82,15 @@ public class FlappyBird extends ApplicationAdapter {
 		if(posicaoInicialVertical > 0 || velocidadeQueda < 0)
 			posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
 
-		// Movimento *verifica se a tela foi tocada*
+		// Toque de tela*
 		if ( Gdx.input.justTouched() ){
 			velocidadeQueda = -10;
+		}
+
+		// Verifca se o cano saiu inteiramente da tela.
+		if (posicaoMovimentoCanoHorizontal < - canoTopoMaior.getWidth() ){
+			posicaoMovimentoCanoHorizontal = larguraDispositivo;
+			alturaEntreCanosRondomica = numerosRandomico.nextInt(400) - 200;
 		}
 
 		//Abertura looop
@@ -64,8 +99,12 @@ public class FlappyBird extends ApplicationAdapter {
 		// img fundo
 		batch.draw( fundo, 0,0, larguraDispositivo, alturaDispositivo );
 
-		// Movimento
-		batch.draw(passaros[ ( int )variacao ], 30, posicaoInicialVertical);
+		// CanoTop
+		batch.draw( canoTopoMaior, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2  + alturaEntreCanosRondomica);
+		batch.draw( canoBaixoMaior, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixoMaior.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRondomica);
+
+		// Movimento bater asas
+		batch.draw(passaros[ ( int )variacao ], 120, posicaoInicialVertical);
 
 		//fechamento
 		batch.end();
